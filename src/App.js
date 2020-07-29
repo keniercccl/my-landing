038ee0ebe78f13a18505bdfc4fe6fff60e15d7f1
keniercccl <keniercccl@gmail.com>
2020-07-28 22:39:39 -0400

    Commit de prueba

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ import Wrapper from './component/Wrapper';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          LANDING PAGE KENIER
-          <Wrapper />
-        </p>
-        
-          Learn React
-        
-      </header>
+      <Wrapper \>
     </div>
   );
 }
